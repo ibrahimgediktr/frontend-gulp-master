30640aa3f30d396cb38fec18bd745db9794796dd
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2021-03-23 12:56:16 +0300

    created email validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,58 @@
 const textHover = document.querySelector('.productive-text-link');
 const burgerMenu = document.querySelector('.burger-menu');
 const navList = document.querySelector('.nav-list');
+const form = document.getElementById('get-started-form');
+const email = document.querySelector('.email');
+const errorSmall = document.querySelector('small');
+const formBtn = document.querySelector('.started-btn');
+const mediaQuery = window.matchMedia('(max-width: 992px)')
 
-burgerMenu.addEventListener('click', function(){
+console.log(formBtn);
+console.log(errorSmall);
+console.log(form);
+
+// Burger menu active and animation
+burgerMenu.addEventListener('click', function () {
     burgerMenu.classList.toggle('burger-animation')
     navList.classList.toggle('nav-active')
 })
 
-textHover.addEventListener('mouseover', function() {
+// Productive link text hover
+textHover.addEventListener('mouseover', function () {
     textHover.classList.add('hover');
 })
-textHover.addEventListener('mouseleave', function() {
+textHover.addEventListener('mouseleave', function () {
     textHover.classList.remove('hover');
+});
+
+// Form validation
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    checkInputs();
 })
+
+function checkInputs() {
+    const emailValue = email.value;
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+
+    if (emailValue.match(pattern)) {
+        errorSmall.innerText = "Email is valid"
+        errorSmall.style.color = "green"
+    }
+    else {
+        errorSmall.innerText = "Please enter a valid email adreess"
+        errorSmall.style.color = "$error"
+    }
+}
+
+
+
+// function setError(input, message){
+//     const formControl
+// }
+
+
 
 // var burgerMenu = document.querySelector('.burger-menu');
 // var navList = document.querySelector('.nav-list');
@@ -33,6 +73,3 @@ textHover.addEventListener('mouseleave', function() {
 //         }
 //     });
 // });
-
-
-
